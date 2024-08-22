e8ad983c02cd898301380d82b233d28128e67975
comicway <129238200+comicway@users.noreply.github.com>
2024-08-21 21:46:07 -0400

    Porbando Flex

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,8 +1,19 @@
 const HeaderScript = () => {
     return (
       <>
-        <div>
-            
+        <div className="flex flex-row">
+            <div className="basis-1/4">
+                <button className="bg-sky-700 w-72 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">BOTON A</button>
+            </div>
+            <div className="basis-1/4">
+                <button className="bg-sky-700 w-72 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">BOTON B</button>
+            </div>
+            <div className="basis-1/4">
+                <button className="bg-sky-700 w-72 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">BOTON C</button>
+            </div>
+            <div className="basis-1/4">
+                <button className="bg-sky-700 w-72 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">BOTON D</button>
+            </div>
         </div>
       </>
     )
