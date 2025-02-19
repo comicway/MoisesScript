5f05631f830439398c55c8af46fbe38a4c554bd8
Moises Contreras <129238200+comicway@users.noreply.github.com>
2025-02-19 05:53:54 -0300

    Formulario de contacto
    Centrar formulario de contacto: ok

--- src/Components/Contacto/Contacto.jsx
+++ src/Components/Contacto/Contacto.jsx
@@ -6,24 +6,26 @@ const Contacto = () => {
                 <h2 className="font-light font-Oswald text-azulbrillante text-[111px]">deja tu mensaje aquí</h2>
             </section>
             <form className="container mx-auto">
-                <div className="grid grid-cols-1 justify-center">
+                <div className="">
                     <label for="nombre" classNams="block text-sm/6 font-medium text-gray-900"></label>
-                    <div>
+                    <div className="flex justify-center">
                         <input placeholder="Nombre" type="text" name="nombre" id="nombre" autocomplete="given-name" className="mt-[135px] max-w-2xl w-full rounded-sm bg-[#1E293B] px-3 py-1.5 text-base text-white font-Inter outline outline-1 -outline-offset-1 outline-fondobtnmenu placeholder:text-azulbrillante focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-azulbrillante sm:text-sm/6"/>
                     </div>
                     <label for="nombre" classNams="block text-sm/6 font-medium text-gray-900"></label>
-                    <div>
+                    <div className="flex justify-center">
                         <input placeholder="Teléfono" type="telt" name="telefono" id="telefono" autocomplete="given-tel" className="mt-[12px] max-w-2xl w-full rounded-sm bg-[#1E293B] px-3 py-1.5 text-base text-white font-Inter outline outline-1 -outline-offset-1 outline-fondobtnmenu placeholder:text-azulbrillante focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-azulbrillante sm:text-sm/6"/>
                     </div>
                     <label for="nombre" classNams="block text-sm/6 font-medium text-gray-900"></label>
-                    <div>
+                    <div className="flex justify-center">
                         <input placeholder="Email" type="email" name="email" id="email" autocomplete="given-email" className="mt-[12px] max-w-2xl w-full rounded-sm bg-[#1E293B] px-3 py-1.5 text-base text-white font-Inter outline outline-1 -outline-offset-1 outline-fondobtnmenu placeholder:text-azulbrillante focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-azulbrillante sm:text-sm/6"/>
                     </div>
                     <label for="nombre" classNams="block text-sm/6 font-medium text-gray-900"></label>
-                    <div>
+                    <div className="flex justify-center">
                         <textarea placeholder="Mensaje" name="about" id="about" rows="6" className="mt-[12px] max-w-2xl w-full rounded-sm bg-[#1E293B] px-3 py-1.5 text-base text-white font-Inter outline outline-1 -outline-offset-1 outline-fondobtnmenu placeholder:text-azulbrillante focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-azulbrillante sm:text-sm/6"></textarea>
                     </div>
-                    <button type="submit" class="mt-[21px] rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Enviar</button>
+                    <div className="flex justify-center">
+                        <button type="submit" class="h-[38px] max-w-2xl w-full mt-[21px] border-2 border-azulbrillante font-bold font-Inter text-azulbrillante text-[15px]">Enviar</button>
+                    </div>
                 </div>
              </form>
         </>
