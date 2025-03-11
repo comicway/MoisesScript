fa1d48b6304ea5a4ca43c987fe0a8419d9dd20e6
Moises Contreras <129238200+comicway@users.noreply.github.com>
2025-03-11 12:43:55 -0300

    Inicio libreria Formik
    Inicio libreria Formik

--- src/Components/Contacto/Contacto.jsx
+++ src/Components/Contacto/Contacto.jsx
@@ -6,27 +6,25 @@ const Contacto = () => {
                 <h2 className="font-light font-Oswald text-azulbrillante text-[111px]">deja tu mensaje aquí</h2>
             </section>
             <form className="container mx-auto">
-                <div className="">
-                    <label for="nombre" classNams="block text-sm/6 font-medium text-gray-900"></label>
+                    <label htmlFor="nombre" className="block text-sm/6 font-medium text-gray-900"></label>
                     <div className="flex justify-center">
-                        <input placeholder="Nombre" type="text" name="nombre" id="nombre" autocomplete="given-name" className="mt-[135px] max-w-2xl w-full rounded-sm bg-[#1E293B] px-3 py-1.5 text-base text-white font-Inter outline outline-1 -outline-offset-1 outline-fondobtnmenu placeholder:text-azulbrillante focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-azulbrillante sm:text-sm/6"/>
+                        <input placeholder="Nombre" type="text" name="nombre" id="nombre" autoComplete="given-name" className="mt-[135px] max-w-2xl w-full rounded-sm bg-[#1E293B] px-3 py-1.5 text-base text-white font-Inter outline outline-1 -outline-offset-1 outline-fondobtnmenu placeholder:text-azulbrillante focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-azulbrillante sm:text-sm/6"/>
                     </div>
-                    <label for="nombre" classNams="block text-sm/6 font-medium text-gray-900"></label>
+                    <label htmlFor="nombre" className="block text-sm/6 font-medium text-gray-900"></label>
                     <div className="flex justify-center">
-                        <input placeholder="Teléfono" type="telt" name="telefono" id="telefono" autocomplete="given-tel" className="mt-[12px] max-w-2xl w-full rounded-sm bg-[#1E293B] px-3 py-1.5 text-base text-white font-Inter outline outline-1 -outline-offset-1 outline-fondobtnmenu placeholder:text-azulbrillante focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-azulbrillante sm:text-sm/6"/>
+                        <input placeholder="Teléfono" type="telt" name="telefono" id="telefono" autoComplete="given-tel" className="mt-[12px] max-w-2xl w-full rounded-sm bg-[#1E293B] px-3 py-1.5 text-base text-white font-Inter outline outline-1 -outline-offset-1 outline-fondobtnmenu placeholder:text-azulbrillante focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-azulbrillante sm:text-sm/6"/>
                     </div>
-                    <label for="nombre" classNams="block text-sm/6 font-medium text-gray-900"></label>
+                    <label htmlFor="nombre" className="block text-sm/6 font-medium text-gray-900"></label>
                     <div className="flex justify-center">
-                        <input placeholder="Email" type="email" name="email" id="email" autocomplete="given-email" className="mt-[12px] max-w-2xl w-full rounded-sm bg-[#1E293B] px-3 py-1.5 text-base text-white font-Inter outline outline-1 -outline-offset-1 outline-fondobtnmenu placeholder:text-azulbrillante focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-azulbrillante sm:text-sm/6"/>
+                        <input placeholder="Email" type="email" name="email" id="email" autoComplete="given-email" className="mt-[12px] max-w-2xl w-full rounded-sm bg-[#1E293B] px-3 py-1.5 text-base text-white font-Inter outline outline-1 -outline-offset-1 outline-fondobtnmenu placeholder:text-azulbrillante focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-azulbrillante sm:text-sm/6"/>
                     </div>
-                    <label for="nombre" classNams="block text-sm/6 font-medium text-gray-900"></label>
+                    <label htmlFor="nombre" className="block text-sm/6 font-medium text-gray-900"></label>
                     <div className="flex justify-center">
                         <textarea placeholder="Mensaje" name="about" id="about" rows="6" className="mt-[12px] max-w-2xl w-full rounded-sm bg-[#1E293B] px-3 py-1.5 text-base text-white font-Inter outline outline-1 -outline-offset-1 outline-fondobtnmenu placeholder:text-azulbrillante focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-azulbrillante sm:text-sm/6"></textarea>
                     </div>
                     <div className="flex justify-center">
-                        <button type="submit" class="h-[38px] max-w-2xl w-full mt-[21px] border-2 border-azulbrillante font-bold font-Inter text-azulbrillante text-[15px]">Enviar</button>
+                        <button type="submit" className="h-[38px] max-w-2xl w-full mt-[21px] border-2 border-azulbrillante font-bold font-Inter text-azulbrillante text-[15px]">Enviar</button>
                     </div>
-                </div>
              </form>
         </>
     )
