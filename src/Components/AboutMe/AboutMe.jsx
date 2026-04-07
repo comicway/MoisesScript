fccb1f54d6927df8700758e1472df621c8ef0b24
comicway <mises.xiaomi@gmail.com>
2026-04-07 12:40:59 -0400

    Cambios importantes en el home

--- src/Components/AboutMe/AboutMe.jsx
+++ src/Components/AboutMe/AboutMe.jsx
@@ -9,7 +9,7 @@ const AboutMe = () => {
                         </div>
                         <div className="p-5 sm:col-span-3">
                             <p className="font-Inter text-letrablanca text-lg mb-4">Desde el 2011, he diseñado páginas web para diversos mercados, como tecnología, salud y entretenimiento. Mi especialidad es el diseño UI y la maquetación frontend, utilizando diversas tecnologías; React JS, Tailwindcss, Bootstrap y Material Design.</p>
-                            <p className="font-Inter text-letrablanca text-lg mb-4">En los últimos años, me he centrado en el comercio electrónico, gestionando grandes sitios web en Wordpress y Shopify. Además, he aplicado metodologías como Lean UX, Sprint, Design Thinking y Growth Marketing, transformando mis proyectos digitales de manera positiva y rentable.</p>
+                            <p className="font-Inter text-letrablanca text-lg mb-4">En los últimos años, me he centrado en el comercio electrónico, gestionando grandes sitios web en Wordpress y Shopify. Además, he aplicado metodologías como Lean UX, Design Thinking y Growth Marketing, transformando mis proyectos digitales de manera positiva y rentable.</p>
                             <p className="font-Inter text-letrablanca text-lg mb-4">Me considero un apasionado del diseño de interfaces gráficas centradas en el usuario, sin descuidar los objetivos de la organización. Soy una persona tranquila y paciente, dispuesto a ayudar en lo que necesites.</p>
                             <p className="font-Inter text-letrablanca text-lg">Te invito a revisar mi currículum y, posteriormente, explorar mi portafolio con mis proyectos más impactantes y recientes.</p>
                             <button className="w-full mt-5">
--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,61 +3,61 @@ import { Link } from "react-router-dom"
 const FooterScript = () => {
     return (
         <>
-        <section className="sm:mt-[80px] pt-[72px] pb-[65px] border-t-4 border-azulbrillante">
-            <div className="container mx-auto px-4">
-                <div className="grid grid-cols-1 sm:grid-cols-6 gap-5">
-                    <div className="sm:col-span-5">
-                        <h3 className="text-white font-medium font-Oswald text-[42px] leading-snug">Este es el momento de contactarme</h3>
-                        <p className="text-white font-medium font-Inter text-[16px]">¡No dudes en ponerte en contacto conmigo o simplemente saludarme!</p>
+            <section className="sm:mt-[80px] pt-[72px] pb-[65px] border-t-4 border-azulbrillante">
+                <div className="container mx-auto px-4">
+                    <div className="grid grid-cols-1 sm:grid-cols-6 gap-5">
+                        <div className="sm:col-span-5">
+                            <h3 className="text-white font-medium font-Oswald text-[42px] leading-snug">Este es el momento de contactarme</h3>
+                            <p className="text-white font-medium font-Inter text-[16px]">¡No dudes en ponerte en contacto conmigo o simplemente saludarme!</p>
+                        </div>
+                        <a href="https://wa.me/56977658072">
+                            <button className="w-[159px] h-[54px] bg-azulbrillante font-bold font-Inter text-[18px] mt-[26px]">Escríbeme</button>
+                        </a>
                     </div>
-                    <a href="https://wa.me/56956542379">
-                        <button className="w-[159px] h-[54px] bg-azulbrillante font-bold font-Inter text-[18px] mt-[26px]">Escríbeme</button>
-                    </a>
                 </div>
-            </div> 
-        </section>
-        <footer className="pt-[50px] pb-[35px] bg-black px-4">
-            <div className="container mx-auto">
-                <div className="grid grid-cols-1 sm:grid-cols-4 gap-5 pb-[16px]">
-                    <div className="sm:col-span-2">
-                        <div className="flex flex-col">
-                            <img src="./asset/img/logomoisesscript.svg" alt="logo moises.script" className="self-start w-auto sm:h-auto sm:mb-[122px]" />
-                            <div className="flex mt-[20px] sm:mt-auto gap-3">
-                                <a href="https://github.com/comicway" target="blank"><img src="./asset/img/ico-github.svg" alt="GitHub" /></a>
-                                <a href="https://www.instagram.com/moises.script/" target="blank"><img src="./asset/img/ico-instagram.svg" alt="Instagram" /></a>
-                                <a href="https://www.threads.net/@moises.script" target="blank"><img src="./asset/img/ico-threads.svg" alt="Threads" /></a>
+            </section>
+            <footer className="pt-[50px] pb-[35px] bg-black px-4">
+                <div className="container mx-auto">
+                    <div className="grid grid-cols-1 sm:grid-cols-4 gap-5 pb-[16px]">
+                        <div className="sm:col-span-2">
+                            <div className="flex flex-col">
+                                <img src="./asset/img/logomoisesscript.svg" alt="logo moises.script" className="self-start w-auto sm:h-auto sm:mb-[122px]" />
+                                <div className="flex mt-[20px] sm:mt-auto gap-3">
+                                    <a href="https://github.com/comicway" target="blank"><img src="./asset/img/ico-github.svg" alt="GitHub" /></a>
+                                    <a href="https://www.instagram.com/moises_script/" target="blank"><img src="./asset/img/ico-instagram.svg" alt="Instagram" /></a>
+                                    <a href="https://www.threads.net/@moises_script" target="blank"><img src="./asset/img/ico-threads.svg" alt="Threads" /></a>
+                                </div>
                             </div>
                         </div>
+                        <div>
+                            <h4 className="text-white font-Oswald font-bold text-[20px]">Contacto</h4>
+                            <ul className="text-linkoff font-Inter font-normal text-[16px] mt-[35px] leading-loose">
+                                <li><a href="tel:+56977658072">+56 9 77 65 80 72</a></li>
+                                <li><a href="mailto:info@moises-script.cl">info@moises-script.cl</a></li>
+                                <li>Santiago Centro</li>
+                                <li><a href="http://github.com/comicway">github.com/comicway</a></li>
+                            </ul>
+                        </div>
+                        <div>
+                            <h4 className="text-white font-Oswald font-bold text-[20px]">Mapa del Sitio</h4>
+                            <ul className="text-linkoff font-Inter font-normal text-[16px] mt-[35px] leading-loose">
+                                <Link to='/'><li>Home CV</li></Link>
+                                <Link to='/proyectos'><li>Proyectos</li></Link>
+                                <Link to='/blog'><li>Blog</li></Link>
+                                <Link to='/contacto'><li>Contacto</li></Link>
+                            </ul>
+                        </div>
                     </div>
-                    <div>
-                        <h4 className="text-white font-Oswald font-bold text-[20px]">Contacto</h4>
-                        <ul className="text-linkoff font-Inter font-normal text-[16px] mt-[35px] leading-loose">
-                            <li><a href="tel:+56956542379">+56 9 56 54 23 79</a></li>
-                            <li><a href="mailto:info@moises-script.cl">info@moises-script.cl</a></li>
-                            <li>Santiago Centro</li>
-                            <li><a href="http://github.com/comicway">github.com/comicway</a></li>
-                        </ul>
-                    </div>
-                    <div>
-                        <h4 className="text-white font-Oswald font-bold text-[20px]">Mapa del Sitio</h4>
-                        <ul className="text-linkoff font-Inter font-normal text-[16px] mt-[35px] leading-loose">
-                            <Link to='/'><li>Home CV</li></Link>
-                            <Link to='/proyectos'><li>Proyectos</li></Link>
-                            <Link to='/blog'><li>Blog</li></Link>
-                            <Link to='/contacto'><li>Contacto</li></Link>
-                        </ul>
+                    <div className="grid grid-cols-1 sm:grid-cols-6 gap-5 border-t border-custom-gray pt-[30px]">
+                        <div className="sm:col-span-5">
+                            <p className="font-Inter font-normal text-white">Diseñado y desarrollado por <span className="font-bold text-naranjo">Moisés Contreras</span></p>
+                        </div>
+                        <div>
+                            <p className="font-Inter font-normal text-white">Copyright ©2025</p>
+                        </div>
                     </div>
                 </div>
-                <div className="grid grid-cols-1 sm:grid-cols-6 gap-5 border-t border-custom-gray pt-[30px]">
-                    <div className="sm:col-span-5">
-                        <p className="font-Inter font-normal text-white">Diseñado y desarrollado por <span className="font-bold text-naranjo">Moisés Contreras</span></p>
-                    </div>
-                    <div>
-                        <p className="font-Inter font-normal text-white">Copyright ©2025</p>
-                    </div>
-                </div>
-            </div>
-        </footer>
+            </footer>
         </>
     )
 }
