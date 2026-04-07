265124e24da8968c6ae63fec5f10b765261404d5
comicway <mises.xiaomi@gmail.com>
2026-04-07 16:05:07 -0400

    pequeno ajuste en proyecto

--- src/Components/Proyectos/Proyectos.jsx
+++ src/Components/Proyectos/Proyectos.jsx
@@ -14,7 +14,7 @@ const ProyectoPage = () => {
         if (filtroCategory === 'inactivo') return 'block';
         return filtroCategory === category ? 'block' : 'hidden';
     };
-    
+
     const isButtonActive = (category) => {
         if (filtroCategory === 'inactivo') return 'border';
         return filtroCategory === category ? 'border-4 bg-fondobtnmenu' : 'border';
@@ -30,15 +30,41 @@ const ProyectoPage = () => {
                     <button onClick={toggleVideo} className={`rounded-full border-colorborder font-Oswald font-normal text-[12px] sm:text-[22px] text-white p-1 w-[274px] mr-2 ${isButtonActive('video')}`}>VIDEOJUEGOS</button>
                 </div>
                 <div className="grid grid-cols-1 sm:grid-cols-3 gap-9 pb-[70px] sm:pb-[0px]">
+                    <div className={`mt-[44px] sm:mt-[90px] ${isCategoryVisible('frontend')}`}>
+                        <img className="rounded-[20px] h-[330px] w-full object-cover" src="./asset/img/portadaMegaBook.png" alt="Aplicación móvil para fomentar los hábitos de lectura" />
+                        <h2 className="text-white font-Oswald text-[27px] mt-[45px] border-b border-azulbrillante pb-[28px]">App MegaBook</h2>
+                        <div className="flex justify-between items-end mt-3">
+                            <span className="text-white font-Oswald text-[18px]">Frontend, Diseño UX / UI</span>
+                            <div className="flex justify-between">
+                                <Link to='/toomics'>
+                                    <button className="text-azulbrillante font-Inter font-medium text-[20px]">Ver proyecto</button>
+                                </Link>
+                                <img className="pl-1" src="./asset/img/vectorflecha.svg" alt="" />
+                            </div>
+                        </div>
+                    </div>
+                    <div className={`mt-[44px] sm:mt-[90px] ${isCategoryVisible('frontend')}`}>
+                        <img className="rounded-[20px] h-[330px] w-full object-cover" src="./asset/img/portadaroverapp.png" alt="" />
+                        <h2 className="text-white font-Oswald text-[27px] mt-[45px] border-b border-azulbrillante pb-[28px]">Rover Photo App</h2>
+                        <div className="flex justify-between items-end mt-3">
+                            <span className="text-white font-Oswald text-[18px]">Frontend</span>
+                            <div className="flex justify-between">
+                                <Link to='/roverphotoapp'>
+                                    <button className="text-azulbrillante font-Inter font-medium text-[20px]">Ver proyecto</button>
+                                </Link>
+                                <img className="pl-1" src="./asset/img/vectorflecha.svg" alt="" />
+                            </div>
+                        </div>
+                    </div>
                     <div className={`mt-[90px] ${isCategoryVisible('ux')}`}>
                         <img className="rounded-[20px] h-[330px] w-full object-cover" src="./asset/img/portadacomicapp.png" alt="portada comic app" />
                         <h2 className="text-white font-Oswald text-[27px] mt-[45px] border-b border-azulbrillante pb-[28px]">Comic App</h2>
                         <div className="flex justify-between items-end mt-3">
                             <span className="text-white font-Oswald text-[18px]">Diseño UX / UI</span>
                             <div className="flex justify-between">
-                            <Link to='/comicapp'>
-                                <button className="text-azulbrillante font-Inter font-medium text-[20px]">Ver proyecto</button>
-                            </Link>
+                                <Link to='/comicapp'>
+                                    <button className="text-azulbrillante font-Inter font-medium text-[20px]">Ver proyecto</button>
+                                </Link>
                                 <img className="pl-1" src="./asset/img/vectorflecha.svg" alt="" />
                             </div>
                         </div>
@@ -49,9 +75,9 @@ const ProyectoPage = () => {
                         <div className="flex justify-between items-end mt-3">
                             <span className="text-white font-Oswald text-[18px]">Diseño UX / UI</span>
                             <div className="flex justify-between">
-                            <Link to='/toomics'>
-                                <button className="text-azulbrillante font-Inter font-medium text-[20px]">Ver proyecto</button>
-                            </Link>
+                                <Link to='/toomics'>
+                                    <button className="text-azulbrillante font-Inter font-medium text-[20px]">Ver proyecto</button>
+                                </Link>
                                 <img className="pl-1" src="./asset/img/vectorflecha.svg" alt="" />
                             </div>
                         </div>
@@ -63,19 +89,6 @@ const ProyectoPage = () => {
                             <span className="text-white font-Oswald text-[18px]">Videojuegos</span>
                             <div className="flex justify-between">
                                 <Link to='/odisyshoot'>
-                                    <button className="text-azulbrillante font-Inter font-medium text-[20px]">Ver proyecto</button>
-                                </Link>
-                                    <img className="pl-1" src="./asset/img/vectorflecha.svg" alt="" />
-                            </div>
-                        </div>
-                    </div>
-                    <div className={`mt-[44px] sm:mt-[90px] ${isCategoryVisible('frontend')}`}>
-                        <img className="rounded-[20px] h-[330px] w-full object-cover" src="./asset/img/portadaroverapp.png" alt="" />
-                        <h2 className="text-white font-Oswald text-[27px] mt-[45px] border-b border-azulbrillante pb-[28px]">Rover Photo App</h2>
-                        <div className="flex justify-between items-end mt-3">
-                            <span className="text-white font-Oswald text-[18px]">Frontend</span>
-                            <div className="flex justify-between">
-                                <Link to='/roverphotoapp'>
                                     <button className="text-azulbrillante font-Inter font-medium text-[20px]">Ver proyecto</button>
                                 </Link>
                                 <img className="pl-1" src="./asset/img/vectorflecha.svg" alt="" />
