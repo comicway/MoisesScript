4164455f998b834487e3d39719fd7e423e266f5c
comicway <moises.xiaomi@gmail.com>
2026-09-01 12:20:15 -0400

    cambio meta descriopcion del home

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,8 @@
 import HomePage from "../Components/Home/Home";
 
 export const metadata = {
-  title: "Diseño UX / UI | Moisés Contreras",
-  description: "Explora cómo transformamos ideas en experiencias digitales. Especialistas en diseño interactivo, prototipado y desarrollo frontend.",
+  title: "Servicios de Diseño UX / UI | Moisés Contreras",
+  description: "Servicios profesionales de diseño UX/UI para crear experiencias digitales excepcionales. Diseñamos interfaces intuitivas y atractivas que mejoran la interacción del usuario y potencian tu marca.",
   alternates: {
     canonical: "/",
   },
